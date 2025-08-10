5fd1826109e98156b8976e845504ff1c935f71e0
SamsulAlomLaskar <itsmesamsulalom@gmail.com>
2025-08-11 00:00:35 +0530

    custom page not found & notFound function

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReviewNotFound = () => {
+  return <div>Review not found as the set limit is exceeded</div>;
+};
+
+export default ReviewNotFound;
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { notFound } from "next/navigation";
 
 const ProductReview = ({
   params,
 }: {
   params: { reviewId: string; productId: string };
 }) => {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Product Review Page with dynamic Nested Route having product Id:
