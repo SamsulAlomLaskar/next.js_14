6947f62b6044cce0514e4e319020413e64a32908
SamsulAlomLaskar <itsmesamsulalom@gmail.com>
2025-08-10 15:50:24 +0530

    catch-all-segment / optional c-a-s

---
+++ src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Docs = ({ params }: { params: { slug: string[] } }) => {
+  {
+    if (params.slug?.length === 2) {
+      return (
+        <h1>
+          Viewing Docs on Home Page for features {params.slug[0]} and concept{" "}
+          {params.slug[1]}
+        </h1>
+      );
+    } else if (params.slug?.length === 1) {
+      return <h1>Viewing Docs on Home Page for feature {params.slug[0]}</h1>;
+    } else {
+      return <h1>Viewing Docs on Home Page</h1>;
+    }
+  }
+};
+export default Docs;
