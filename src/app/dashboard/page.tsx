fe1a1628e64d1e809ed326367a614f169d066866
SamsulAlomLaskar <itsmesamsulalom@gmail.com>
2025-08-11 16:35:07 +0530

    file colocation & private folders

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import BarChart from "./BarChart";
+
+const DashBoard = () => {
+  return (
+    <>
+      <h1>DashBoard</h1>
+      <BarChart />{" "}
+    </>
+  );
+};
+
+export default DashBoard;
--- src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ReviewNotFound = () => {
-  return <div>Review not found as the set limit is exceeded</div>;
+  return <h1>Review not found as the set limit is exceeded</h1>;
 };
 
 export default ReviewNotFound;
