26b60f52b5b6177cee683fc314641c9ce492aeb0
SamsulAlomLaskar <itsmesamsulalom@gmail.com>
2025-08-14 16:25:08 +0530

    error handling using error.tsx file

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { notFound } from "next/navigation";
 
+const generateRandomId = (count: number) => {
+  return Math.floor(Math.random() * count);
+};
+
 const ProductReview = ({
   params,
 }: {
   params: { reviewId: string; productId: string };
 }) => {
+  const randomId = generateRandomId(2);
+
+  if (randomId === 1) {
+    throw new Error("Error occurred while fetching the review");
+  }
+
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
+
   return (
     <h1>
       Product Review Page with dynamic Nested Route having product Id:
